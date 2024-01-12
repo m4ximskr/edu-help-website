c724f1a643ae2d1cbe25148a19ea6ac60638e083
Skr <m4ximskr@gmail.com>
2024-01-12 22:22:53 +0200

    fixed send service

--- src/app/shared/services/send-mail.service.ts
+++ src/app/shared/services/send-mail.service.ts
@@ -7,32 +7,24 @@ import {of, throwError, timer} from 'rxjs';
 })
 export class SendMailService {
 
-  headers = { 'Content-Type': 'application/json' };
+  private headers = { 'Content-Type': 'application/json' };
+  private apiUrl = 'https://api.eduhelp.lv'
+  // private apiUrl = 'http://localhost:3000'
 
   constructor(private http: HttpClient) { }
 
-  // http://localhost:3000/send-mail
-  // https://api.eduhelp.lv/send-mail
-  // http://localhost:3000/send-order-mail
-
   sendOrderMail(data) {
-    // return of(null)
-
-    // return this.http.post('https://api.eduhelp.lv/send-order-mail', {params: data, headers: this.headers});
-    return timer(2000);
+    return this.http.post(`${this.apiUrl}/send-order-mail`, {params: data, headers: this.headers});
+    // return timer(2000);
   }
 
   sendQuestionMail(data) {
-    // return of(null)
-    // return throwError('ee')
-    // return this.http.post('https://api.eduhelp.lv/send-question-mail', {params: data});
-    return timer(2000);
+    return this.http.post(`${this.apiUrl}/send-question-mail`, {params: data});
+    // return timer(2000);
   }
 
   sendTutoringOrderMail(data) {
-    return this.http.post('https://api.eduhelp.lv/send-tutoring-mail', {params: data, headers: this.headers});
-    // console.log(data);
-    // return of(null);
+    return this.http.post(`${this.apiUrl}/send-tutoring-mail`, {params: data, headers: this.headers});
     // return timer(2000);
   }
 
